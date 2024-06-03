9dceba1fb271537640ac402ccd1ffc1499c79a0f
jazed2 <jazze.martine@gmail.com>
2024-06-03 08:55:54 +0530

    Added new colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,37 @@
 return {
 	{
+		"pbrisbin/vim-colors-off",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"robertmeta/nofrils",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"rafi/awesome-vim-colorschemes",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"bluz71/vim-moonfly-colors",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
 		"sam4llis/nvim-tundra",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
