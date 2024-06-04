a44ae442a2c19f07209e79ec21c19665209a1873
jazed2 <jazze.martine@gmail.com>
2024-06-05 01:02:32 +0530

    Added new colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,32 @@
 return {
 	{
-		"bluz71/vim-moonfly-colors",
+		"Mofiqul/adwaita.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		lazy = false,
+		-- configure and set on startup
+		config = function()
+			vim.g.adwaita_darker = false -- for darker version
+			vim.g.adwaita_transparent = false -- makes the background transparent
+		end,
+	},
+
+	{
+		"bluz71/vim-moonfly-colors",
+	},
+
+	{
+		"savq/melange-nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		lazy = false,
+	},
+
+	{
+		"comfysage/evergarden",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		lazy = false,
+		opts = {
+			contrast_dark = "hard", -- 'hard'|'medium'|'soft'
+			overrides = {}, -- add custom overrides
+		},
 	},
 }
