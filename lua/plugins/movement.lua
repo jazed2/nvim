ab389f48f0f0444880171df8a629bae9614af80a
jazed2 <jazze.martine@gmail.com>
2024-06-07 23:17:55 +0530

    Added 'ThePrimagen/harpoon' for easy file navigation

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -16,4 +16,63 @@ return {
 			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 		end,
 	},
+
+	{ -- Primagen's harpoon for faster file navigation
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			local keymap = vim.keymap.set
+			harpoon:setup({})
+
+			-- basic telescope configuration
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			-- Keymaps
+			keymap("n", "<C-e>", function()
+				toggle_telescope(harpoon:list())
+			end, { desc = "Open harpoon window" })
+			keymap("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			keymap("n", "<C-h>", function()
+				harpoon:list():select(1)
+			end)
+			keymap("n", "<C-t>", function()
+				harpoon:list():select(2)
+			end)
+			keymap("n", "<C-n>", function()
+				harpoon:list():select(3)
+			end)
+			keymap("n", "<C-s>", function()
+				harpoon:list():select(4)
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			keymap("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			keymap("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
 }
