614afad8a68f934af8fff3c52c5d71ef5df8ae38
jazed2 <jazze.martine@gmail.com>
2024-06-09 16:03:38 +0530

    replaced harpoon with arrow

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -17,5 +17,12 @@ return {
 		end,
 	},
 
+	{ -- File and buffer bookmarks
+		"otavioschwanck/arrow.nvim",
+		opts = {
+			show_icons = true,
+			leader_key = ";", -- Recommended to be a single key
+			buffer_leader_key = "m", -- Per Buffer Mappings
+		},
 	},
 }
