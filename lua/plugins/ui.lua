51484964b45f497d608dcfe4207f1634bb6eee69
jazed2 <jazze.martine@gmail.com>
2024-06-11 08:25:55 +0530

    Added statusline plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -362,4 +362,33 @@ return {
 			end,
 		},
 	},
+
+	{ -- Statusline
+		"bluz71/nvim-linefly",
+		config = function()
+			vim.g.linefly_options = { -- opts
+				separator_symbol = "⎪",
+				progress_symbol = "↓",
+				active_tab_symbol = "*",
+				git_branch_symbol = "",
+				error_symbol = "E",
+				warning_symbol = "W",
+				information_symbol = "I",
+				ellipsis_symbol = "…",
+				tabline = false,
+				winbar = true,
+				with_file_icon = true,
+				with_git_branch = true,
+				with_git_status = true,
+				with_diagnostic_status = true,
+				with_session_status = true,
+				with_attached_clients = true,
+				with_lsp_status = false,
+				with_macro_status = false,
+				with_search_count = false,
+				with_spell_status = false,
+				with_indent_status = false,
+			}
+		end,
+	},
 }
