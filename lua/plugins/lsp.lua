35394a107649ce65fe9ca12a5952754aed00e6d4
jazed2 <jazze.martine@gmail.com>
2024-06-18 08:29:54 +0530

    Added onsails/lspkind.nvim for VSCode style lsp pictograms

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -265,11 +265,13 @@ return {
 			--  into multiple repos for maintenance purposes.
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"onsails/lspkind.nvim", -- VSCode style pictograms
 		},
 		config = function()
 			-- See `:help cmp`
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
 			luasnip.config.setup({})
 
 			cmp.setup({
@@ -331,6 +333,21 @@ return {
 					{ name = "luasnip" },
 					{ name = "path" },
 				},
+
+				format = lspkind.cmp_format({
+					mode = "symbol", -- show only symbol annotations
+					maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+					-- can also be a function to dynamically calculate max width such as
+					-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+					-- The function below will be called before any actual modifications from lspkind
+					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+					before = function(entry, vim_item)
+						return vim_item
+					end,
+				}),
 			})
 		end,
 	},
