1d481f417750e476dad05e473de17dc952a9a919
jazed2 <jazze.martine@gmail.com>
2024-06-18 08:46:55 +0530

    New Additions

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -8,7 +8,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({ import = "plugins" })
 
 return {
-	ui = {
+	ui = { -- Fallback icons when no nerdfont is installed
 		icons = vim.g.have_nerd_font and {} or {
 			cmd = "⌘",
 			config = "🛠",
@@ -25,22 +25,43 @@ return {
 			lazy = "💤 ",
 		},
 	},
-	rtp = {
-		disabled_plugins = {
-			"gzip",
-			-- "matchit",
-			-- "matchparen",
-			"netrwPlugin",
-			"rplugin",
-			"tarPlugin",
-			"tohtml",
-			"tutor",
-			"zipPlugin",
+
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
+
+	checker = {
+		enabled = false, -- automatically check for plugin updates
+		concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+		notify = true, -- get a notification when new updates are found
+		frequency = 3600, -- check for updates every hour
+		check_pinned = false, -- check for pinned packages that can't be updated
+	},
+
+	performance = {
+		cache = { enabled = true },
+
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				"netrwPlugin",
+				"rplugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
 		},
 	},
-	change_detection = { notify = false },
-	checker = {
-		enabled = true, -- automatically check for plugin updates
-		notify = false, -- get a notification when new updates are found
+
+	readme = {
+		enabled = true,
+		root = vim.fn.stdpath("state") .. "/lazy/readme",
+		files = { "README.md", "lua/**/README.md" },
+		-- only generate markdown helptags for plugins that dont have docs
+		skip_if_doc_exists = true,
 	},
 }
