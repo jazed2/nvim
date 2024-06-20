b1809309e9b883828d9ad2c750468b41b644eb62
jazed2 <jazze.martine@gmail.com>
2024-06-20 09:06:00 +0530

    New Linters and formatters added

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -88,7 +88,10 @@ return {
 		config = function()
 			local lint = require("lint")
 			lint.linters_by_ft = {
-				markdown = { "markdownlint" },
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
 				json = { "jsonlint" },
 			}
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,6 +77,15 @@ return {
 				}
 			end,
 			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
