96ba804cd506ba54e8398e332ad257a162e5ef24
jazed2 <jazze.martine@gmail.com>
2024-06-20 23:47:59 +0530

    Added new/Moved existing git related plugins to git.lua

---
+++ lua/plugins/git.lua
@@ -0,0 +1,33 @@
+return {
+	-- Git related plugins
+	{ "tpope/vim-fugitive", "tpope/vim-rhubarb" },
+
+	{ -- LazyGit integration with Telescope
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{
+				"<leader>lg",
+				":LazyGit<Return>",
+				silent = true,
+				noremap = true,
+			},
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+}
