359d521dd9537b834e522563d040c4b2425082f3
jazze <jazze@bazzitearchroot.bazzite>
2025-04-04 07:31:48 +0530

    Added Plugin to add local config

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -311,4 +311,24 @@ return {
 		"chamindra/marvim",
 		event = "BufReadPre",
 	},
+
+	{ -- Local configs
+		"klen/nvim-config-local",
+		config = function()
+			require("config-local").setup({
+				-- Default options (optional)
+
+				-- Config file patterns to load (lua supported)
+				config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+
+				-- Where the plugin keeps files data
+				hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+				autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+				commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalDeny)
+				silent = false, -- Disable plugin messages (Config loaded/denied)
+				lookup_parents = false, -- Lookup config files in parent directories
+			})
+		end,
+	},
 }
