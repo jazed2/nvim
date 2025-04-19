9d624652e0eda2662ead18b01b52315a1f7b32c1
jazze <jazze@bazzitearchroot.bazzite>
2025-04-19 16:55:59 +0530

    Removed less used plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -279,34 +279,6 @@ return {
 		vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", { noremap = true, silent = true }),
 	},
 
-	{ -- Spawn floating terminal inside nvim
-		"akinsho/toggleterm.nvim",
-		event = "BufRead",
-		version = "*",
-		config = function()
-			require("toggleterm").setup({
-				size = 20,
-				open_mapping = [[<c-t>]],
-				shade_filetypes = {},
-				shade_terminals = true,
-				shading_factor = 2,
-				start_in_insert = true,
-				persist_size = false,
-				direction = "float",
-				close_on_exit = true,
-				shell = vim.o.shell,
-				float_opts = {
-					border = "curved",
-					winblend = 0,
-					highlights = {
-						border = "Normal",
-						background = "Normal",
-					},
-				},
-			})
-		end,
-	},
-
 	{ -- Macro repository to save macros
 		"chamindra/marvim",
 		event = "BufReadPre",
