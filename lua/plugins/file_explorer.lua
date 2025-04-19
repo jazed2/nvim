e1d337e0010ae6b27d9e78f3fb5c50ac6316e8f8
jazze <jazze@bazzitearchroot.bazzite>
2025-04-19 16:55:26 +0530

    Removed NeoTree because not using

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -17,28 +17,4 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
 	},
-
-	{ -- NeoTree
-		"nvim-neo-tree/neo-tree.nvim",
-		event = "BufReadPre",
-		version = "*",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-		cmd = "Neotree",
-		keys = {
-			{ "<leader><TAB>", ":Neotree reveal<CR>", desc = "NeoTree reveal" },
-		},
-		opts = {
-			filesystem = {
-				window = {
-					mappings = {
-						["<leader><TAB>"] = "close_window",
-					},
-				},
-			},
-		},
-	},
 }
