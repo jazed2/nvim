7c252f3a0f5d0f6786ddd790f7d9d120caaa7389
jazze <jazze@bazzitearchroot.bazzite>
2025-04-25 12:21:22 +0530

    Cleanup

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -1,17 +1,24 @@
-local slComponent = {} -- statusline components
-local hi_pattern = "%%#%s#%s%%*"
+local M = {}
+
+local SlComponent = {} -- statusline components
+
+local hiPattern = "%%#%s#%s%%*" -- ergonomics
+-- hiPatterns usage:
+-- hiPattern:format("highlight-group", "text") or hiPattern.format(hiPattern,"highlight-group", "text")
+-- example: hiPattern:format("Search", "text")
+-- for highlight groups, see :h highlight-groups or :Telescope highlights
 
 function _G._statusline_component(name)
-	return slComponent[name]()
+	return SlComponent[name]()
 end
 
-function slComponent.diagnosticStatus()
+function SlComponent.diagnosticStatus()
 	local diagnosis = " ✔ "
 	local diagnosticSeverity = vim.diagnostic.severity
 
 	local errors = #vim.diagnostic.get(0, { severity = diagnosticSeverity.ERROR })
 	if errors > 0 then
-		diagnosis = " E:" .. errors .. " "
+		diagnosis = "%%#Error#%E:" .. errors .. "%* "
 	end
 
 	local warnings = #vim.diagnostic.get(0, { severity = diagnosticSeverity.WARN })
@@ -19,18 +26,53 @@ function slComponent.diagnosticStatus()
 		diagnosis = " W:" .. warnings .. " "
 	end
 
-	return hi_pattern:format("Search", diagnosis)
+	return hiPattern:format("Search", diagnosis)
 end
 
-function slComponent.position()
-	return hi_pattern:format("Search", " %3l:%-2c [%L] ")
+function SlComponent.vcsInfo()
+	local branch = vim.fn.system("git branch --show-current 2&> /dev/null")
+	local fileInfo = vim.b.gitsigns_status_dict
+
+	if branch ~= "" then -- print branch name
+		branch = " " .. branch:gsub("%s+$", "")
+	else
+		branch = ""
+	end
+
+	if not fileInfo or fileInfo.head == "" then
+		return ""
+	end
+
+	local added = fileInfo.added and ("+" .. fileInfo.added .. " ") or ""
+	local changed = fileInfo.changed and ("~" .. fileInfo.changed .. " ") or ""
+	local removed = fileInfo.removed and ("-" .. fileInfo.removed .. " ") or ""
+
+	if fileInfo.added == 0 then
+		added = ""
+	end
+	if fileInfo.changed == 0 then
+		changed = ""
+	end
+	if fileInfo.removed == 0 then
+		removed = ""
+	end
+
+	return table.concat({
+		branch,
+		" ",
+		added,
+		changed,
+		removed,
+		" ",
+	})
 end
 
-function slComponent.getLspServer()
+function SlComponent.lspNames()
 	local bufnr = vim.api.nvim_get_current_buf()
 	local lspClients = vim.lsp.get_clients({ bufnr })
+	local lspNames = ""
 
-	if next(lspClients) == nil then
+	if not next(lspClients) then
 		return ""
 	end
 
@@ -38,61 +80,29 @@ function slComponent.getLspServer()
 	for _, client in pairs(lspClients) do
 		table.insert(names, client.name)
 	end
-	return "  [" .. table.concat(names, " | ") .. "]"
+	lspNames = table.concat(names, " | ")
+
+	-- return hiPattern:format("SlComponent_lspName", lspNames)
+	return lspNames
 end
 
-function slComponent.gitBranch()
-	local printBranch
-
-	local branch = vim.fn.system("git branch --show-current 2&> /dev/null")
-	if branch ~= "" then
-		printBranch = " " .. branch:gsub("%s+$", "")
-	else
-		printBranch = ""
-	end
-
-	return printBranch
-end
-
-function slComponent.vcsStatus()
-	local git_info = vim.b.gitsigns_status_dict
-	if not git_info or git_info.head == "" then
-		return ""
-	end
-	local added = git_info.added and ("+" .. git_info.added .. " ") or ""
-	local changed = git_info.changed and ("~" .. git_info.changed .. " ") or ""
-	local removed = git_info.removed and ("-" .. git_info.removed .. " ") or ""
-	if git_info.added == 0 then
-		added = ""
-	end
-	if git_info.changed == 0 then
-		changed = ""
-	end
-	if git_info.removed == 0 then
-		removed = ""
-	end
-
-	return table.concat({
-		added,
-		changed,
-		removed,
-		" ",
-		"%#Normal#",
-	})
+function SlComponent.position()
+	return hiPattern:format("Search", " %3l:%-2c [%L] ")
 end
 
 local statusline = {
 	'%{%v:lua._statusline_component("diagnosticStatus")%} ',
-	' %{%v:lua._statusline_component("gitBranch")%} ',
-	' %{%v:lua._statusline_component("vcsStatus")%} ',
+	' %{%v:lua._statusline_component("vcsInfo")%} ',
 	"%=",
 	"%F",
 	"%r",
 	"%m",
 	"%=",
-	' %{%v:lua._statusline_component("getLspServer")%} ',
+	' %{%v:lua._statusline_component("lspNames")%} ',
 	'%{%v:lua._statusline_component("position")%} ',
 	" %2p%% ",
 }
 
 vim.o.statusline = table.concat(statusline, "")
+
+return M
