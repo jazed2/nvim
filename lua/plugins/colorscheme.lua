a0d44fa54e590410ad48c6c2a70fe1030e22fb6c
jazze <jazze@bazzitearchroot.bazzite>
2025-04-27 00:04:26 +0530

    Added new coloscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,4 +16,24 @@ return {
 		lazy = false,
 		priority = 1000,
 	},
+
+	{
+		"aktersnurra/no-clown-fiesta.nvim",
+		lazy = false,
+		priority = 100,
+		config = function()
+			require("no-clown-fiesta").setup({
+				styles = {
+					-- You can set any of the style values specified for `:h nvim_set_hl`
+					comments = {},
+					functions = {},
+					keywords = {},
+					lsp = {},
+					match_paren = {},
+					type = {},
+					variables = {},
+				},
+			})
+		end,
+	},
 }
