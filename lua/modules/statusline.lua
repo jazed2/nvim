2e8795dfbff6e9c1649240ac4f544517a89224d9
jazze <jazze@bazzitearchroot.bazzite>
2025-04-27 00:13:21 +0530

    feat: Custom statusline.lua

---
+++ lua/modules/statusline.lua
@@ -0,0 +1,138 @@
+local M = {}
+
+local SlComponent = {} -- statusline components
+
+local hiPattern = "%%#%s#%s%%*" -- ergonomics
+---@param hiPattern string
+-- hiPatterns usage:
+-- hiPattern:format("highlight-group", "text") or hiPattern.format(hiPattern,"highlight-group", "text")
+-- example: hiPattern:format("Search", "text")
+-- for highlight groups, see :h highlight-groups or :Telescope highlights
+
+function _G._statusline_component(name)
+	return SlComponent[name]()
+end
+
+function SlComponent.diagnosticStatus()
+	local diagnosis = " ✔ "
+	local diagnosticSeverity = vim.diagnostic.severity
+
+	local errors = #vim.diagnostic.get(0, { severity = diagnosticSeverity.ERROR })
+	if errors > 0 then
+		diagnosis = "%%#Error#%E:" .. errors .. "%* "
+	end
+
+	local warnings = #vim.diagnostic.get(0, { severity = diagnosticSeverity.WARN })
+	if warnings > 0 then
+		diagnosis = " W:" .. warnings .. " "
+	end
+
+	return hiPattern:format("Search", diagnosis)
+end
+
+function SlComponent.vcsInfo()
+	local branch = vim.fn.system("git branch --show-current 2&> /dev/null")
+	local fileInfo = vim.b.gitsigns_status_dict
+
+	if branch ~= "" then -- print branch name
+		branch = " " .. branch:gsub("%s+$", "")
+	else
+		branch = ""
+	end
+
+	if not fileInfo or fileInfo.head == "" then
+		return ""
+	end
+
+	local added = fileInfo.added and ("+" .. fileInfo.added .. " ") or ""
+	local changed = fileInfo.changed and ("~" .. fileInfo.changed .. " ") or ""
+	local removed = fileInfo.removed and ("-" .. fileInfo.removed .. " ") or ""
+
+	if fileInfo.added == 0 then
+		added = ""
+	end
+	if fileInfo.changed == 0 then
+		changed = ""
+	end
+	if fileInfo.removed == 0 then
+		removed = ""
+	end
+
+	return hiPattern:format(
+		"@markup",
+		table.concat({
+			" ",
+			branch,
+			" ",
+			added,
+			changed,
+			removed,
+			" ",
+		})
+	)
+end
+
+function SlComponent.fileName()
+	function splitAtDelim(filePath, delimiter) -- Make table out of every word separated by the Delimiter == /
+		local pattern = string.format("([^%s]+)", delimiter)
+		local pathComponenets = {}
+
+		for str in filePath:gmatch(pattern) do
+			table.insert(pathComponenets, str)
+		end
+
+		return pathComponenets
+	end
+
+	local filePath = vim.fn.expand("%F")
+
+	if filePath:sub(1, 2) == "~/" then
+		local pathParts = splitAtDelim(filePath:sub(3), "/")
+		if #pathParts >= 3 then
+			local a = pathParts[#pathParts - 2]
+			local b = pathParts[#pathParts - 1]
+			local c = pathParts[#pathParts]
+			return a .. "/" .. b .. "/" .. c
+		else
+			return hiPattern:format("@markup", filePath)
+		end
+	else
+		return hiPattern:format("@markup", filePath)
+	end
+end
+
+function SlComponent.lspNames()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local lspClients = vim.lsp.get_clients({ bufnr })
+	local lspNames = ""
+
+	if not next(lspClients) then
+		return ""
+	end
+
+	local names = {}
+	for _, client in pairs(lspClients) do
+		table.insert(names, client.name)
+	end
+	lspNames = table.concat(names, " | ")
+
+	return hiPattern:format("@markup", lspNames .. " ")
+end
+
+function SlComponent.position()
+	return hiPattern:format("Search", " %3l:%-2c [%2p%%] ")
+end
+
+local statusline = {
+	'%{%v:lua._statusline_component("diagnosticStatus")%}',
+	'%{%v:lua._statusline_component("vcsInfo")%} ',
+	" %=",
+	'%{%v:lua._statusline_component("fileName")%} ',
+	"%=",
+	' %{%v:lua._statusline_component("lspNames")%}',
+	'%{%v:lua._statusline_component("position")%}',
+}
+
+vim.o.statusline = table.concat(statusline, "")
+
+return M
