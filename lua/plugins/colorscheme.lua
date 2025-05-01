67512b42f07952abef4b6f0a36aa11f240df6065
jazze <jazze@bazzitearchroot.bazzite>
2025-05-02 00:31:29 +0530

    chore: test and clean

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,7 @@
 return {
-	{ -- alabaster
-		"p00f/alabaster.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-
 	{ -- rasmus
 		"kvrohit/rasmus.nvim",
 		lazy = false,
 		priority = 1000,
-	},
-
-	{ -- no-clown-fiesta
-		"aktersnurra/no-clown-fiesta.nvim",
-		lazy = false,
-		priority = 100,
-		config = function()
-			require("no-clown-fiesta").setup({
-				styles = {
-					-- You can set any of the style values specified for `:h nvim_set_hl`
-					comments = {},
-					functions = {},
-					keywords = {},
-					lsp = {},
-					match_paren = {},
-					type = {},
-					variables = {},
-				},
-			})
-		end,
 	},
 }
