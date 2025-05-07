834451a73a28a1c751a5d8165df0a65c8334ef2c
jazze <jazze@bazzitearchroot.bazzite>
2025-05-07 09:25:38 +0530

    feat: new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,4 +4,17 @@ return {
 		lazy = false,
 		priority = 1000,
 	},
+
+	{ -- rose-pine
+		"rose-pine/neovim",
+		lazy = false,
+		priority = 1000,
+		name = "rose-pine",
+	},
+
+	{ -- monochrome
+		"kdheepak/monochrome.nvim",
+		lazy = false,
+		priority = 1000,
+	}
 }
