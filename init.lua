f795f74daa24488e4c222685a708db0fafd5563b
jazze <jazze@bazzitearchroot.bazzite>
2025-08-03 00:34:02 +0530

    chore

--- init.lua
+++ init.lua
@@ -2,6 +2,5 @@ require("core.options")
 require("core.keymaps")
 require("core.autocommands")
 require("core.lazy")
-require("modules.statusline")
 
-vim.cmd.colorscheme("randomhue")
+vim.cmd.colorscheme("rasmus")
