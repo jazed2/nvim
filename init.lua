b57632a27cd0822b0f339c887e9bb5f8a5506de7
jazze <jazze@bazzitearchroot.bazzite>
2025-08-03 09:04:33 +0530

    Added custom statusline

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@ require("core.options")
 require("core.keymaps")
 require("core.autocommands")
 require("core.lazy")
+require("modules.statusline")
 
-vim.cmd.colorscheme("rasmus")
+vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE")
+
+vim.cmd.colorscheme("rose-pine-main")
